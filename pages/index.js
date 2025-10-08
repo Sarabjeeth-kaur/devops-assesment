cbc3da5a4afa95c8ad578530a336bf6b11e6605f
Sarabjeeth-kaur <sarabjeethkaur@gmail.com>
2025-10-08 13:08:43 +0530

    added to index

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ export default function Home() {
     <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '20%' }}>
       <h1> DevOps Assessment - Next.js App</h1>
       <p>Containerized and deployed using Docker, GitHub Actions, and Kubernetes (Minikube)</p>
+      <p>Sardarni Sarabjeeth Kaur</p>
+      <p>ദ്ദി（• ˕ •マ.ᐟ</p>
     </div>
   );
 }
